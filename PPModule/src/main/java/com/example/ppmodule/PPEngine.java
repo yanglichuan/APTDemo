5912cc52f1f4be90ce6c0e15773943dbd280ed23
yanglichuan <yanglichuan@ushareit.com>
2021-09-03 19:02:34 +0800

    测试 添加一种登录方式

---
+++ PPModule/src/main/java/com/example/ppmodule/PPEngine.java
@@ -0,0 +1,26 @@
+package com.example.ppmodule;
+
+
+
+
+import androidx.annotation.Keep;
+
+import com.example.basecore.IEngine;
+
+import cn.tim.annotation.DILoginEngine;
+
+
+/**
+ * @author (ylc)
+ * @datetime 2021-09-01 11:16 GMT+8
+ * @email yanglichuan@ushareit.com
+ * @detail :
+ */
+@Keep
+@DILoginEngine
+public class PPEngine implements IEngine {
+    @Override
+    public String getName(){
+        return "fb";
+    }
+}
